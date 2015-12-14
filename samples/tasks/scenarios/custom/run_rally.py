00c8f08e105e18de96fba0d3bff7bd40991be33d
hkumarmk <hkumar@d4devops.org>
2015-12-14 12:29:11 +0530

    Get scenarios from task_args
    This patch changes run_rally.py and scenario.yaml to get the scenarios
    from task args.

--- samples/tasks/scenarios/custom/run_rally.py
+++ samples/tasks/scenarios/custom/run_rally.py
@@ -5,6 +5,7 @@ import os
 import fnmatch
 import subprocess
 import sys
+import yaml
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -12,16 +13,23 @@ THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 def get_context(c):
   return c
 
-def create_rally_input(run_scenarios, fp,
+def create_rally_input(fp, run_scenarios=None,
                        template_file='scenario.yaml',
                        scenario_directrory='scenarios'):
 
     jenv = jinja2.Environment(loader=jinja2.FileSystemLoader(THIS_DIR),
                          trim_blocks=True)
-    scenarios = {
-        scenario_directrory + '/' + x: os.path.splitext(x)[0]
-        for x in os.listdir(scenario_directrory)
-            if os.path.isfile(scenario_directrory+os.sep+x) and fnmatch.fnmatch(scenario_directrory+os.sep+x, '*.yaml')  and os.path.splitext(x)[0] in run_scenarios
+    if run_scenarios:
+        scenarios = {
+            scenario_directrory + '/' + x: os.path.splitext(x)[0]
+            for x in os.listdir(scenario_directrory)
+                if os.path.isfile(scenario_directrory+os.sep+x) and fnmatch.fnmatch(scenario_directrory+os.sep+x, '*.yaml')  and os.path.splitext(x)[0] in run_scenarios
+        }
+    else:
+        scenarios = {
+            scenario_directrory + '/' + x: os.path.splitext(x)[0]
+            for x in os.listdir(scenario_directrory)
+                if os.path.isfile(scenario_directrory+os.sep+x) and fnmatch.fnmatch(scenario_directrory+os.sep+x, '*.yaml')
         }
 
     template = jenv.get_template(template_file)
@@ -42,7 +50,6 @@ def run_rally(template, task_args=None, task_args_file=None):
     elif task_args_file:
         cmd.extend(['--task-args-file', task_args_file])
 
-    print cmd
     p = subprocess.Popen(cmd,
                          stdout=subprocess.PIPE,
                          stderr=subprocess.STDOUT)
@@ -54,6 +61,7 @@ def run_rally(template, task_args=None, task_args_file=None):
 
 
 def main(argv=sys.argv):
+    scenarios = []
     ap = argparse.ArgumentParser(
         description="Run rally tasks")
     ap.add_argument("--task-args", type=str,
@@ -65,19 +73,28 @@ def main(argv=sys.argv):
     args = ap.parse_args()
     if args.task_args and args.task_args_file:
         raise argparse.ArgumentError('Either one of task-args or task-args-file to be provided')
+
     if not args.task_args and not args.task_args_file:
         raise argparse.ArgumentError('Either one of task-args or task-args-file to be provided')
 
+    if args.task_args_file:
+        with open(args.task_args_file, 'r') as stream:
+            scenarios = yaml.load(stream).get('scenarios', [])
+
+    if args.task_args:
+        scenarios = yaml.load(args.task_args).get('scenarios',[])
+
     (fp, file) = tempfile.mkstemp(dir=THIS_DIR)
     try:
-        create_rally_input(['random','create_and_list_networks','create_harish'], fp)
+        create_rally_input(fp, scenarios)
         for out in run_rally(file, task_args=args.task_args, task_args_file=args.task_args_file):
-            #print(out.rstrip('\n').decode("latin"))
             sys.stdout.write(out)
             sys.stdout.flush()
-#            sys.stderr.flush()
     finally:
         os.remove(file)
+
+    return True
+
 
 if __name__ == "__main__":
     sys.exit(not main(sys.argv))
