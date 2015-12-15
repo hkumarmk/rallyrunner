66c018fe6e34265fcce86116b775a5e740e1c9cb
hkumarmk <hkumar@d4devops.org>
2015-12-15 10:33:22 +0530

    run_rally.py to accept and pass scenario specific arguments
    This patch is to add capability to run_rally.py to accept scenario
    specific arguments in same yaml input (task-args/task-args-file), and
    pass appropriate params to individual scenarios.
    
    Changes in run_rally.py
    
    * Added RallyRunner class and added all of those methods to it so that
    they can use instance variables
    * Added methods to extract scenario specific arguments from yaml input
    and to convert to jinja2 macro parameters.
    
    Changes in scenario.yaml
    * ADded code to check if parameters have been given for specific
    scenario and if yes, pass those parameter to the macro
    
    Changes in individual scenarios (random.yaml as example)
    * All scenario specific jinja2 macros should accept the params as macro
    parameters. run_rally.py will generate main scenario jinja template with
    appropriate parameters as it get.
    
    It also deleted create_and_list_security_groups.yaml as it is not being
    used.

--- samples/tasks/scenarios/custom/run_rally.py
+++ samples/tasks/scenarios/custom/run_rally.py
@@ -9,55 +9,81 @@ import yaml
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
-@jinja2.contextfunction
-def get_context(c):
-  return c
 
-def create_rally_input(fp, run_scenarios=None,
-                       template_file='scenario.yaml',
-                       scenario_directrory='scenarios'):
+class RallyRunner():
+    def __init__(self, rally_task_args=None,
+                 template_file='scenario.yaml',
+                 scenario_directory='scenarios'):
+        self.template_file = template_file
+        self.scenario_directory = scenario_directory
+        self.rally_task_args = rally_task_args
+        self.rally_scenarios = self.rally_task_args.get('scenarios', [])
+        self.template = None
 
-    jenv = jinja2.Environment(loader=jinja2.FileSystemLoader(THIS_DIR),
-                         trim_blocks=True)
-    if run_scenarios:
+    @jinja2.contextfunction
+    def get_context(self, c):
+        return c
+
+    def get_scenario_args(self, scenario):
+        """ Return scenario arguments in the format "param1=value1, param2=value2"
+        :param scenario: scenario name
+        :return: scenario arguments, None if no scenario arguments
+        """
+        args = ','.join(["{}='{}'".format(k,v) for k,v in self.rally_task_args.get('scenario_args', {}).get(scenario, {}).iteritems()])
+        return args
+
+    def create_rally_input(self, fp):
+
+        jenv = jinja2.Environment(loader=jinja2.FileSystemLoader(THIS_DIR),
+                                  trim_blocks=True)
+        if self.rally_scenarios:
+            scenario_details = {
+                self.scenario_directory + '/' + x: ({ os.path.splitext(x)[0]: self.get_scenario_args(os.path.splitext(x)[0])} if self.get_scenario_args(os.path.splitext(x)[0])
+                else os.path.splitext(x)[0])
+                for x in os.listdir(self.scenario_directory)
+                if os.path.isfile(self.scenario_directory + os.sep + x) and fnmatch.fnmatch(
+                self.scenario_directory + os.sep + x, '*.yaml') and os.path.splitext(x)[0] in self.rally_scenarios
+                }
+        else:
+            scenario_details = {
+                self.scenario_directory + '/' + x: ({ os.path.splitext(x)[0]: self.get_scenario_args(os.path.splitext(x)[0])} if self.get_scenario_args(os.path.splitext(x)[0])
+                else os.path.splitext(x)[0])
+                for x in os.listdir(self.scenario_directory)
+                if os.path.isfile(self.scenario_directory + os.sep + x) and fnmatch.fnmatch(
+                self.scenario_directory + os.sep + x, '*.yaml')
+                }
+
         scenarios = {
-            scenario_directrory + '/' + x: os.path.splitext(x)[0]
-            for x in os.listdir(scenario_directrory)
-                if os.path.isfile(scenario_directrory+os.sep+x) and fnmatch.fnmatch(scenario_directrory+os.sep+x, '*.yaml')  and os.path.splitext(x)[0] in run_scenarios
-        }
-    else:
-        scenarios = {
-            scenario_directrory + '/' + x: os.path.splitext(x)[0]
-            for x in os.listdir(scenario_directrory)
-                if os.path.isfile(scenario_directrory+os.sep+x) and fnmatch.fnmatch(scenario_directrory+os.sep+x, '*.yaml')
+            i: (''.join(j.keys()) if isinstance(j, dict) else j)
+            for i, j in scenario_details.iteritems()
         }
 
-    template = jenv.get_template(template_file)
-    template.globals['context'] = get_context
-    template.globals['callable'] = callable
-    context = { 'templates': scenarios }
+        template = jenv.get_template(self.template_file)
+        template.globals['context'] = self.get_context
+        template.globals['callable'] = callable
+        context = {'scenario_details': scenario_details, 'scenarios': scenarios}
+        footer = (template.render(**context))
+        if fp:
+            os.write(fp, footer)
+        os.close(fp)
 
-    footer = (template.render(**context))
-    if fp:
-        os.write(fp, footer)
-    os.close(fp)
+    # rally task start scenario.yaml --task-args "{cxt_tenants: 1, cxt_users_per_tenant: 4, cxt_network: true, base_network_load_objects: 30, load_type: serial, times: 1, scenarios: [create_and_list_networks, random]}"
+    @staticmethod
+    def run_rally(template_file, task_args=None, task_args_file=None):
+        cmd = ['rally', 'task', 'start', template_file]
+        if task_args:
+            cmd.extend(['--task-args', task_args])
+        elif task_args_file:
+            cmd.extend(['--task-args-file', task_args_file])
 
-#rally task start scenario.yaml --task-args "{cxt_tenants: 1, cxt_users_per_tenant: 4, cxt_network: true, base_network_load_objects: 30, load_type: serial, times: 1, scenarios: [create_and_list_networks, random]}"
-def run_rally(template, task_args=None, task_args_file=None):
-    cmd = ['rally', 'task', 'start', template]
-    if task_args:
-        cmd.extend(['--task-args', task_args])
-    elif task_args_file:
-        cmd.extend(['--task-args-file', task_args_file])
-
-    p = subprocess.Popen(cmd,
-                         stdout=subprocess.PIPE,
-                         stderr=subprocess.STDOUT)
-    while(True):
-        next_out = p.stdout.readline()
-        yield (next_out)
-        if next_out == '' and p.poll() != None:
-            break
+        p = subprocess.Popen(cmd,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.STDOUT)
+        while True:
+            next_out = p.stdout.readline()
+            yield (next_out)
+            if next_out == '' and p.poll() != None:
+                break
 
 
 def main(argv=sys.argv):
@@ -79,15 +105,17 @@ def main(argv=sys.argv):
 
     if args.task_args_file:
         with open(args.task_args_file, 'r') as stream:
-            scenarios = yaml.load(stream).get('scenarios', [])
+            rally_task_args = yaml.load(stream)
 
     if args.task_args:
-        scenarios = yaml.load(args.task_args).get('scenarios',[])
+        rally_task_args = yaml.load(args.task_args)
+
+    rr = RallyRunner(rally_task_args=rally_task_args)
 
     (fp, file) = tempfile.mkstemp(dir=THIS_DIR)
     try:
-        create_rally_input(fp, scenarios)
-        for out in run_rally(file, task_args=args.task_args, task_args_file=args.task_args_file):
+        rr.create_rally_input(fp)
+        for out in rr.run_rally(file, task_args=args.task_args, task_args_file=args.task_args_file):
             sys.stdout.write(out)
             sys.stdout.flush()
     finally:
